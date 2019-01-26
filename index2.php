1fa4cd4e1505a00e5c2328ba034338c16fa9b3a5
arifin999 <37579009+arifin999@users.noreply.github.com>
2019-01-26 09:55:21 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <!-- Fontawesome 5 Free -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!-- Swiper Slider -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.6/css/swiper.min.css">
+    <!-- Font Google -->
+    <link href="https://fonts.googleapis.com/css?family=Muli|Poppins:300,400,900" rel="stylesheet">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <title>SMP Negeri 2 Binjai</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="assets/img/tutwuri_warna.png" alt="" width="50">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><i class="fa fa-home"></i> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sekolah
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Visi &amp; Misi</a>
+                <a class="dropdown-item" href="#">Struktur</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Gallery
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><i class="fa fa-images"></i> Foto</a>
+                <a class="dropdown-item" href="#"><i class="fa fa-video"></i> Video</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pendaftaran</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Hubungi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="root">
+      <!-- New Nav -->
+      <div class="nav-standar">
+        <div class="container">
+          <div class="col-md-12">
+            <div class="nav-img-standar">
+              <img src="assets/img/tutwuri_warna.png" alt="" class="img-fluid" width="100">
+            </div>
+            <div class="nav-content-standar">
+              <h2>SMP Negeri 2 Binjai</h2>
+              <span>Jl. Sultan Hasanuddin No.5-6, Satria, Binjai Kota, Kota Binjai, Sumatera Utara 20741</span>
+              <ul>
+                <li><i class="fa fa-chalkboard-teacher"></i> Guru: 67</li>
+                <li><i class="fa fa-male"></i> Siswa: 465</li>
+                <li><i class="fa fa-female"></i> Siswi: 552</li>
+                <li><i class="fa fa-book"></i> Kurikulum: K-13</li>
+                <li>Akses Internet: <i class="fa fa-check-circle"></i> </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Nav -->
+
+      <!-- Ads -->
+      <div id="ads">
+        <div class="ads_bayangan">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-5">
+                <h1 class="ads_content">
+                  <small>&copy;</small>
+                  <b>The Shoddy Science Behind most Pregnancy Advice</b>
+                </h1>
+                <p class="text-uppercase">Semangat tetap semangat</p>
+              </div>
+              <div class="col-md-6 offset-1 news-content">
+                <p class="title">Berita Hangat</p>
+                <div class="swiper-container swiper-news">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                          <div class="card">
+                            <div class="card">
+                              <img src="assets/img/header/20170320_084343.jpg" class="card-img-top swiper-lazy" alt="Kepala Sekolah">
+                              <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                              <div class="card-body">
+                                <h5 class="card-title">Pendidikan merupakan sebuah keharusan.</h5>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                          <div class="card">
+                            <div class="card">
+                              <img src="assets/img/header/20170320_083814.jpg" class="card-img-top swiper-lazy" alt="Kepala Sekolah">
+                              <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                              <div class="card-body">
+                                <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                          <div class="card">
+                            <div class="card">
+                              <img src="assets/img/header/20170320_083930.jpg" class="card-img-top swiper-lazy" alt="Kepala Sekolah">
+                              <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                              <div class="card-body">
+                                <h5 class="card-title">Duis aute irure dolor in reprehenderit in voluptate.</h5>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                          <div class="card">
+                            <div class="card">
+                              <img src="assets/img/header/20170320_083930.jpg" class="card-img-top swiper-lazy" alt="Kepala Sekolah">
+                              <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                              <div class="card-body">
+                                <h5 class="card-title">Duis aute irure dolor in reprehenderit in voluptate.</h5>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Ands -->
+
+      <!-- Header -->
+      <header id="header">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9">
+              <!-- Slider main container -->
+              <div class="col-md-12">
+                <div class="swiper-container swiper-header">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                          <div class="swiper-zoom-container">
+                            <img src="assets/img/header/20170320_083801.jpg" alt="" class="img-fluid">
+                          </div>
+                        </div>
+                        <div class="swiper-slide">
+                          <div class="swiper-zoom-container">
+                            <img src="assets/img/header/20170320_083814.jpg" alt="" class="img-fluid">
+                          </div>
+                        </div>
+                        <div class="swiper-slide">
+                          <div class="swiper-zoom-container">
+                            <img src="assets/img/header/20170320_083850.jpg" alt="" class="img-fluid">
+                          </div>
+                        </div>
+                    </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- If we need navigation buttons -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                    <!-- If we need scrollbar -->
+                    <div class="swiper-scrollbar"></div>
+                </div>
+              </div>
+              <!-- Artikel -->
+
+            </div>
+            <div class="col-md-3">
+              <div class="card kepsek-profile">
+                <img src="assets/img/kepsek.jpeg" class="card-img-top" alt="Kepala Sekolah">
+                <div class="card-body">
+                  <h5 class="card-title">Eka Mutia Khairuma</h5>
+                  <p class="card-text">Pendidikan merupakan sebuah keharusan yang dilakukan untuk ...</p>
+                  <a href="#" class="btn btn-primary">Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+    <!-- Swiper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.6/js/swiper.min.js"></script>
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
